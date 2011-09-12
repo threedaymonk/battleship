559d190d3dd1688ebadef4896cfc8cc8523dbc82
Paul Battley <pbattley@gmail.com>
2011-09-12 17:22:57 +0100

    Add Stupid and Human players for demonstration purposes.

---
+++ players/human_player.rb
@@ -0,0 +1,21 @@
+class HumanPlayer
+  def name
+    "Human Player"
+  end
+
+  def new_game
+    [5, 4, 3, 3, 2].map{ |length|
+      puts "ship of length #{length}"
+      puts "co-ordinates of top left (x,y)?"
+      x, y = $stdin.gets.split(",").map{ |a| a.strip.to_i }
+      puts "orientation (\"a\" for across or \"d\" for down)?"
+      orientation = $stdin.gets.strip == "a" ? :across : :down
+      [x, y, length, orientation]
+    }
+  end
+
+  def take_turn(state)
+    puts "co-ordinates (x,y)?"
+    x, y = $stdin.gets.split(",").map{ |a| a.strip.to_i }
+  end
+end
---
+++ players/stupid_player.rb
@@ -0,0 +1,19 @@
+class StupidPlayer
+  def name
+    "Stupid Player"
+  end
+
+  def new_game
+    [
+      [0, 0, 5, :across],
+      [0, 1, 4, :across],
+      [0, 2, 3, :across],
+      [0, 3, 3, :across],
+      [0, 4, 2, :across]
+    ]
+  end
+
+  def take_turn(state)
+    [rand(10), rand(10)]
+  end
+end
