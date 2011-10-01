01fa1e17fb584fe41eec9a26c80dca50a461d369
Paul Battley <pbattley@gmail.com>
2011-10-01 15:49:49 +0100

    Added a second stupit player with different behaviour for clarity.

---
+++ players/stupid_player_2.rb
@@ -0,0 +1,19 @@
+class StupidPlayer2
+  def name
+    "Stupid Player 2"
+  end
+
+  def new_game
+    [
+      [5, 0, 5, :across],
+      [5, 1, 4, :across],
+      [5, 2, 3, :across],
+      [5, 3, 3, :across],
+      [5, 4, 2, :across]
+    ]
+  end
+
+  def take_turn(state, ships_remaining)
+    [rand(10), rand(10)]
+  end
+end
