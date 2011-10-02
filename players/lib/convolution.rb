38612c9769182dc5488f8149b8c78028ec0c047f
Paul Battley <pbattley@gmail.com>
2011-10-02 21:24:50 +0100

    Fewer loops in convolution => faster.

--- players/lib/convolution.rb
+++ players/lib/convolution.rb
@@ -1,30 +1,28 @@
 class Convolution
   def initialize(state, kernel, &scorer)
-    @state  = state
-    @kernel = kernel
-    @scorer = scorer
+    @state       = state
+    @flat_kernel = kernel.flatten
+    @scorer      = scorer
+    @offset      = kernel.length / 2
   end
 
   def at(x, y)
-    convolve(sample(x, y), @kernel)
+    convolve(flat_sample(x, y), @flat_kernel)
   end
 
 private
-  def sample(x, y)
-    offset = @kernel.length / 2
-    ((y - offset) .. (y + offset)).map{ |yy|
+  def flat_sample(x, y)
+    ((y - @offset) .. (y + @offset)).map{ |yy|
       row = @state[yy] || []
-      ((x - offset) .. (x + offset)).map{ |xx|
+      ((x - @offset) .. (x + @offset)).map{ |xx|
         @scorer.call(row[xx])
       }
-    }
+    }.flatten
   end
 
-  def convolve(sample, kernel)
-    sample.zip(kernel).inject(0){ |sum, (srow, krow)|
-      srow.zip(krow).inject(sum){ |sum, (s, k)|
-        sum + s * k
-      }
+  def convolve(flat_sample, flat_kernel)
+    flat_sample.zip(flat_kernel).inject(0){ |sum, (s, k)|
+      sum + s * k
     }
   end
 end
