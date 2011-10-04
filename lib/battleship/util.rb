6cff07ccf796994daab1e55c722dc06304799a5e
Paul Battley <pbattley@gmail.com>
2011-10-04 21:36:58 +0100

    Socket timeouts appear not to work on OS X.

--- lib/battleship/util.rb
+++ lib/battleship/util.rb
@@ -13,19 +13,25 @@ module Battleship
         }
     end
 
-    def wait_for_socket(host, port, timeout=1)
-      socket  = Socket.new(:INET, :STREAM)
-      address = Socket.pack_sockaddr_in(port, host)
-      optval = [timeout, 0].pack("l_2")
-      socket.setsockopt :SOCKET, :RCVTIMEO, optval
-      socket.setsockopt :SOCKET, :SNDTIMEO, optval
+    if RUBY_PLATFORM =~ /linux/i
+      def wait_for_socket(host, port, timeout=1)
+        socket  = Socket.new(:INET, :STREAM)
+        address = Socket.pack_sockaddr_in(port, host)
+        optval = [timeout, 0].pack("l_2")
+        socket.setsockopt :SOCKET, :RCVTIMEO, optval
+        socket.setsockopt :SOCKET, :SNDTIMEO, optval
 
-      loop do
-        begin
-          socket.connect(address)
-          return
-        rescue SystemCallError => e
+        loop do
+          begin
+            socket.connect(address)
+            return
+          rescue SystemCallError => e
+          end
         end
+      end
+    else
+      def wait_for_socket(*args)
+        sleep 1
       end
     end
 
