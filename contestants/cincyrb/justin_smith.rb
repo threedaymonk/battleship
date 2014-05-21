5b6c57178d22a0224998b2114f351579e3013f40
Justin W. Smith <smithj73@nku.edu>
2014-05-21 16:04:51 -0400

    A working player

--- contestants/cincyrb/justin_smith.rb
+++ contestants/cincyrb/justin_smith.rb
@@ -1,6 +1,9 @@
 class JustinSmithPlayer
+
+  PROB = 2.0/3.0
+
   def name
-    "Justin Smith"
+    'Justin Smith'
   end
 
   def new_game
@@ -14,15 +17,125 @@ class JustinSmithPlayer
   end
 
   def take_turn(state, ships_remaining)
-    [rand(10), rand(10)]
+    ary = []
+
+    (0...Board.size).each do |x|
+      (0...Board.size).each do |y|
+        ary << Pos.new(x, y, state)
+      end
+    end
+
+    ary.delete_if {|x| x.value == 0 }
+    ary.sort!
+    ary.reverse!
+
+    #puts ary[0..10].inspect
+
+    # Probabilistically select move over geometric distribution
+
+    i = 0
+    while true
+      unless rand > PROB
+        pos =  ary[i % ary.length]
+        puts "State: #{state[pos.x][pos.y]}"
+        puts "Pos: #{pos.inspect}"
+        return [pos.y, pos.x]
+      end
+      i += 1
+    end
+
+  end
+end
+
+
+class Pos
+  attr_reader :value, :x, :y
+
+  def initialize(x, y, state)
+    @x = x
+    @y = y
+    @value = 1
+    compute_value(state)
+  end
+
+  def compute_value(state)
+
+    # If we guessed it before, just skip it.
+    if state[@x][@y] != :unknown
+      @value = 0
+      return
+    end
+
+    # Check column
+    above = 0
+    (@x-1).downto(0) do |x|
+      if state[x][@y] == :unknown
+        above += 1
+      else
+        break
+      end
+    end
+    below = 0
+    (@x+1).upto(Board.size-1) do |x|
+      if state[x][@y] == :unknown
+        below += 1
+      else
+        break
+      end
+    end
+    # Add the smaller of the two
+    @value += ((above < below) ? above : below)
+
+
+    # Check row
+    left = 0
+    (@y-1).downto(0) do |y|
+      if state[@x][y] == :unknown
+        left += 1
+      else
+        break
+      end
+    end
+    right = 0
+    (@y+1).upto(Board.size-1) do |y|
+      if state[@x][y] == :unknown
+        right += 1
+      else
+        break
+      end
+    end
+    # Add the smaller of the two
+    @value += ((left < right) ? left : right)
+
+    # Check neighbors for hit
+    if @x > 0 && state[@x-1][@y] == :hit
+      @value += 10
+    end
+
+    if @x < Board.size-1 && state[@x+1][@y] == :hit
+      @value += 10
+    end
+
+    if @y > 0 && state[@x][@y-1] == :hit
+      @value += 10
+    end
+
+    if @y < Board.size-1 && state[@x][@y+1] == :hit
+      @value += 10
+    end
+
+  end
+
+  def <=>(other)
+    @value <=> other.value
   end
 end
 
 
 class Ship
-  attr_reader :row, :col, :length, :orientation
+  attr_reader :x, :y, :length, :orientation
 
-  def initialize x, y, length, orientation
+  def initialize(x, y, length, orientation)
     @x = x
     @y = y
     @length = length
@@ -41,9 +154,9 @@ class Ship
     end
   end
 
-  def collision? board
-    each_position.any? do |x,y|
-        board.occupied? x, y
+  def collision?(board)
+    each_position.any? do |x, y|
+      board.occupied? x, y
     end
   end
 
@@ -51,7 +164,7 @@ class Ship
     [@x, @y, @length, @orientation]
   end
 
-  def Ship.randomly_place length, board
+  def Ship.randomly_place(length, board)
     while true
       orientation = [:across, :down].sample
       case @orientation
@@ -82,16 +195,16 @@ class Board
     @occupied = {}
   end
 
-  def add_ship ship
-    ship.each_position do |x,y|
-      @occupied[[x,y]] = ship
+  def add_ship(ship)
+    ship.each_position do |x, y|
+      @occupied[[x, y]] = ship
     end
     ship
   end
 
-  def occupied? x, y
+  def occupied?(x, y)
     return true unless (0...@@size) === x && (0...@@size) === y
-    @occupied[[x,y]]
+    @occupied[[x, y]]
   end
 
   def ships
--- spec/justin_smith_spec.rb
+++ spec/justin_smith_spec.rb
@@ -9,7 +9,7 @@ describe 'Ship' do
 
   describe '#each_position' do
 
-    it "should return an enumerable when called without a block" do
+    it 'should return an enumerable when called without a block' do
       expect(ship1.each_position).to be_an(Enumerator)
     end
 
@@ -23,9 +23,9 @@ describe 'Ship' do
   end
 end
 
-describe "Board" do
+describe 'Board' do
 
-  describe "#add_ship" do
+  describe '#add_ship' do
     let(:ship1) {Ship.new 1, 4, 5, :across}
     let(:ship2) {Ship.new 2, 3, 4, :down}
 
@@ -33,12 +33,12 @@ describe "Board" do
       @board1 = Board.new
     end
 
-    it "should return the ship" do
+    it 'should return the ship' do
       @board1.add_ship(ship1).should be(ship1)
     end
   end
 
-  describe "#occupied?" do
+  describe '#occupied?' do
     let(:ship1) {Ship.new 1, 4, 5, :across}
     let(:ship2) {Ship.new 2, 3, 4, :down}
 
@@ -46,7 +46,7 @@ describe "Board" do
       @board1 = Board.new
     end
 
-    it "should mark ship positions a occupied" do
+    it 'should mark ship positions a occupied' do
       expect(@board1).to respond_to(:add_ship)
       @board1.add_ship(ship1).should be_a(Ship)
       ship1.each_position do |x,y|
@@ -54,7 +54,7 @@ describe "Board" do
       end
     end
 
-    it "should return true when out of bounds" do
+    it 'should return true when out of bounds' do
       expect(@board1.occupied?(Board.size, 0)).to be(true)
       expect(@board1.occupied?(0, Board.size)).to be(true)
       expect(@board1.occupied?(0, -1)).to be(true)
@@ -63,14 +63,14 @@ describe "Board" do
     end
   end
 
-  describe "#ships" do
+  describe '#ships' do
     let(:ship1) {Ship.new 1, 4, 5, :across}
 
     before do
       @board1 = Board.new
     end
 
-    it "should return the ships added to the board" do
+    it 'should return the ships added to the board' do
       @board1.add_ship(ship1).should be_a(Ship)
       expect(@board1.ships).to eq([ship1])
     end
@@ -89,29 +89,29 @@ describe 'Ship' do
       @board1.add_ship(ship1)
     end
 
-    it "should detect collisions" do
+    it 'should detect collisions' do
       expect(ship2.collision?(@board1)).to be_true
     end
 
-    it "should not falsely report collisions" do
+    it 'should not falsely report collisions' do
       expect(ship3.collision?(@board1)).to be_false
     end
   end
 end
 
 
-describe "Board" do
+describe 'Board' do
 
   before do
     @board1 = Board.new
   end
 
-  describe "::randomly_place" do
+  describe '::randomly_place' do
     before do
       @lengths =  [5, 4, 3, 3, 2]
     end
 
-    it "should not generate any collisions" do
+    it 'should not generate any collisions' do
       @lengths.each do |length|
         Ship.randomly_place(length, @board1)
       end
@@ -124,7 +124,78 @@ describe "Board" do
       end
       expect(count).to eq(@lengths.inject(:+))
     end
+  end
+end
 
+describe 'JustinSmithPlayer' do
+  before do
+    @player = JustinSmithPlayer.new
   end
 
+  describe '#new_game' do
+    let(:ships) { @player.new_game.map{|x| Ship.new *x } }
+
+    it 'should return 5 ships' do
+      expect(ships).to have_exactly(5).items
+    end
+
+    it 'should not allow collisions' do
+      board = Board.new
+      board.add_ship ships[0]
+      ships[1...ships.length].each do |ship|
+        expect(ship.collision? board).to be_false
+        board.add_ship ship
+      end
+    end
+
+  end
+end
+
+describe 'Pos' do
+  before do
+    @state = []
+    Board.size.times do
+      @state << [:unknown] * Board.size
+    end
+  end
+
+  describe '#value' do
+
+    it 'should value squares away from the edges' do
+      expect(Pos.new(1, 1, @state).value).to be > Pos.new(0, 0, @state).value
+      expect(Pos.new(2, 2, @state).value).to be > Pos.new(1, 1, @state).value
+      expect(Pos.new(3, 3, @state).value).to be > Pos.new(2, 2, @state).value
+      expect(Pos.new(4, 4, @state).value).to be > Pos.new(3, 3, @state).value
+    end
+
+    it 'should highly value squares neighboring hits' do
+      @state[0][1] = :hit
+      #puts @state.inspect
+      expect(Pos.new(0, 0, @state).value).to be > Pos.new(4, 4, @state).value
+      expect(Pos.new(1, 1, @state).value).to be > Pos.new(0, 0, @state).value
+    end
+  end
+end
+
+describe 'JustinSmithPlayer' do
+  before do
+    @player = JustinSmithPlayer.new
+    @state = []
+    Board.size.times do
+      @state << [:unknown] * Board.size
+    end
+  end
+
+  describe '#take_turn' do
+    it 'should prefer squares neighboring hits' do
+      @state[1][1] = :hit
+
+      count = 0
+      8.times do
+        selection = @player.take_turn(@state, 5)
+        count += 1 if [ [0, 1], [1, 0], [1, 2], [2, 1] ].index(selection)
+      end
+      expect(count).to be > 4
+    end
+  end
 end
