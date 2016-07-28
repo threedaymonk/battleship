91cfdc9ed3dcffdb859de326460b0a17da1b0e65
Michael Rauh <michaelrauh@MichaelRauh.attlocal.net>
2016-07-28 18:39:13 -0400

    player returns a random guess for position of opponent

---
+++ players/seek_and_destroy/player.rb
@@ -0,0 +1,23 @@
+class SeekAndDestroy
+  def name
+    "Seek and Destroy"
+  end
+
+  def new_game
+    [
+      [5, 0, 5, :across],
+      [5, 1, 4, :across],
+      [5, 2, 3, :across],
+      [5, 3, 3, :across],
+      [5, 4, 2, :across]
+    ]
+  end
+
+  def take_turn(state, ships_remaining)
+    get_random_position
+  end
+
+  def get_random_position
+    [rand(10), rand(10)]
+  end
+end
--- players/seek_and_destroy/test.rb
+++ players/seek_and_destroy/test.rb
@@ -1,8 +1,26 @@
 require "minitest/autorun"
+require_relative "./player.rb"
+class SeekAndDestroyTest < MiniTest::Unit::TestCase
 
-class BoardTest < MiniTest::Unit::TestCase
+  def test_that_untrained_player_will_attack_randomly
+    player = SeekAndDestroy.new
 
-  def test_nothing
-    assert_equal(true, false)
+    ships_remaining = [5, 4, 3, 3, 2]
+    state = []
+
+    10.times do |row|
+      column = []
+      10.times do |thing|
+        column << :unknown
+      end
+      state << column
+    end
+
+    def player.get_random_position
+      return [0, 0]
+    end
+
+    turn = player.take_turn(state, ships_remaining)
+    assert_equal(turn, [0, 0])
   end
 end
