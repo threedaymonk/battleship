65eb9aae7c446d7ab98604d390e282b1b19af1ef
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-09-25 15:13:17 -0400

    initial clumping logic working for one clump

--- players/seek_and_destroy/clumper.rb
+++ players/seek_and_destroy/clumper.rb
@@ -2,10 +2,39 @@ class Clumper
   def clump(state)
     clumps = []
     coordinates = get_hit_coordinates(state)
-    coordinates.each do |coordinate|
-      clumps << coordinates
-    end
+    if coordinates.size > 0
+    center = coordinates[0]
+    clump = []
+    find_clumps(coordinates, center, clump)
+    clumps << clump
+  end
     clumps
+  end
+
+  def find_clumps(coordinates, center, clump)
+    coordinates -= [center]
+    clump << center
+    right = [center[0] + 1, center[1]]
+    down = [center[0], center[1] + 1]
+    left = [center[0] - 1, center[1]]
+    up = [center[0], center[1] - 1]
+
+    if coordinates.include?(right)
+      coordinates -= [right]
+      find_clumps(coordinates, right, clump)
+    end
+    if coordinates.include?(down)
+      coordinates -= [down]
+      find_clumps(coordinates, down, clump)
+    end
+    if coordinates.include?(left)
+      coordinates -= [right]
+      find_clumps(coordinates, left, clump)
+    end
+    if coordinates.include?(up)
+      coordinates -= [up]
+      find_clumps(coordinates, up, clump)
+    end
   end
 
   private
--- players/seek_and_destroy/clumper_test.rb
+++ players/seek_and_destroy/clumper_test.rb
@@ -26,4 +26,25 @@ class ClumperTest < MiniTest::Unit::TestCase
     found = @clumper.clump(@state).size
     assert_equal(1, found)
   end
+
+  def test_that_clump_returned_by_single_hit_has_coordinate_of_hit
+    @state[0][0] = :hit
+    found = @clumper.clump(@state)[0]
+    expected = [[0, 0]]
+    assert_equal(expected, found)
+  end
+
+  def test_that_one_clump_is_returned_when_there_are_two_adjacent_hits
+    @state[0][0] = :hit
+    @state[0][1] = :hit
+    found = @clumper.clump(@state).size
+    assert_equal(1, found)
+  end
+
+  def test_that_clump_returned_by_two_adjacent_hits_has_coordinates_of_hits
+    @state[0][0] = :hit
+    @state[0][1] = :hit
+    found = @clumper.clump(@state)[0]
+    assert_equal([[0, 0], [0, 1]], found)
+  end
 end
