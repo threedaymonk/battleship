80536d9264e4e7531ca9920c1d12bfaac7bd2510
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-10-24 21:14:43 -0400

    added snapshot feature so that the player can save game state

--- players/seek_and_destroy.rb
+++ players/seek_and_destroy.rb
@@ -1,4 +1,7 @@
 class SeekAndDestroy
+  require 'yaml'
+  
+  @snapshot = nil
   def name
     "Seek and Destroy"
   end
@@ -11,9 +14,16 @@ class SeekAndDestroy
       [0, 3, 3, :across],
       [0, 4, 2, :across]
     ]
+    most_recent = Dir['snapshot_*.yml'].map{|str| str.gsub('snapshot_', '').to_i}.max || 0
+    @snapshot = File.open("snapshot_#{most_recent + 1}.yml", 'w+')
+    @snapshot.write("")
   end
 
   def take_turn(state, ships_remaining)
+    unless @snapshot.nil?
+      YAML.dump(state, @snapshot)
+      @snapshot.close
+    end
     [rand(10), rand(10)]
   end
 end
--- spec/seek_and_destroy_spec.rb
+++ spec/seek_and_destroy_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 require_relative '../players/seek_and_destroy'
 
+
 describe 'player seek and destroy' do
 
   let(:seek_and_destroy) { SeekAndDestroy.new }
 
-  it 'attacks randomly when untrained' do
-    coords = []
+  let(:state){
     state = []
-    ships_remaining = [5, 4, 3, 3, 2]
-
     10.times do |row|
       column = []
       10.times do |thing|
@@ -17,11 +15,42 @@ describe 'player seek and destroy' do
       end
       state << column
     end
+    state
+  }
+
+  let(:ships_remaining){
+    [5, 4, 3, 3, 2]
+  }
+
+  it 'attacks randomly when untrained' do
+    coords = []
 
     1000.times do
-      coords << SeekAndDestroy.new.take_turn(state, ships_remaining)
+      coords << seek_and_destroy.take_turn(state, ships_remaining)
     end
     expect(coords.uniq.size).to eq 100
+  end
+
+  it "creates a snapshot file when a new game starts" do
+    seek_and_destroy.new_game
+    expect(File.exist?('snapshot_1.yml')).to be true
+    File.delete 'snapshot_1.yml'
+  end
+
+  it "creates a new file if there is already a training file" do
+    File.open("snapshot_1.yml", 'w+'){ |file| file.write("")}
+    seek_and_destroy.new_game
+    expect(File.exist?('snapshot_2.yml')).to be true
+    File.delete 'snapshot_1.yml'
+    File.delete 'snapshot_2.yml'
+  end
+
+  it 'writes state to a snapshot when it takes a turn' do
+    seek_and_destroy.new_game
+    seek_and_destroy.take_turn(state, ships_remaining)
+    coordinates = YAML.load_file('snapshot_1.yml')
+    expect(state).to eq coordinates
+    File.delete 'snapshot_1.yml'
   end
 
 end
